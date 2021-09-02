a4c7c93fff9e4fbaf78f0d6b4142e8bcc09510c6
Christopher Arbelo <christopherarbelo@gmail.com>
2021-09-02 14:10:39 -0400

    Modifiy travelers input name to passengers

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,11 +4,14 @@ class FlightsController < ApplicationController
   def home
     @airports = Airport.all.map { |airport| ["#{airport.location} (#{airport.code})", airport.id]}
     @trips = [["One Way", "one-way"]]
-    @travelers = [['1 traveler', 1], ['2 travelers', 2], ['3 travelers', 3], ['4 travelers', 4]]
+    @passengers = [['1 traveler', 1], ['2 travelers', 2], ['3 travelers', 3], ['4 travelers', 4]]
   end
 
   def flights
-    @flights = Flight.includes(:aircraft, :origin, :destination).where(flight_search_params)
+    @flight_search_params = flight_search_params
+    @flights = Flight.includes(:aircraft, :origin, :destination).where(flight_search_basic(flight_search_params))
+
+    flash[:alert] = 'There are no flights for that search!' and redirect_to root_path if @flights.none?
   end
 
   private
@@ -36,6 +39,10 @@ class FlightsController < ApplicationController
   end
 
   def flight_search_params
-    params.permit(:origin, :destination, :date)
+    params.permit(:origin, :destination, :date, :trip, :passengers)
+  end
+
+  def flight_search_basic(flight_params)
+    flight_params.select {|key, _v| [:aircraft, :origin, :destination].include? key }
   end
 end
